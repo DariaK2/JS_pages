c013503c17c74cd4e342dd3b1c22ebe98ac4e895
DariaK2 <54099968+DariaK2@users.noreply.github.com>
2023-11-06 20:24:42 +0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,13 +1,17 @@
 const node_for_click = document.getElementById("for_click")
 
 function find_edit(){
-    const secondName  = document.getElementsByTagName('span')[1]
-    console.log(secondName.innerText)
-    secondName.innerText = 'Kovalenko'
+    const lastName  = document.getElementsByTagName('span')[1]
+    console.log(lastName.innerText)
+    lastName.innerText = 'Kovalenko'
 
     const firstName  = document.getElementsByTagName('span')[2]
     console.log(firstName.innerText)
     firstName.innerText = 'Daria'
+
+    const middleName  = document.getElementsByTagName('span')[3]
+    console.log(middleName.innerText)
+    middleName.innerText = 'Dmitrievna'
 
 }
 
