41d6aa17d3dc81867cb80b7d47ff904c265e7c5b
DariaK2 <54099968+DariaK2@users.noreply.github.com>
2023-11-08 00:40:15 +0300

    Update abc.js

--- js/abc.js
+++ js/abc.js
@@ -26,3 +26,28 @@ const elementD = document.getElementById('d');
 
 const elementVerify = document.getElementById('verify');
 elementVerify.addEventListener('click', verify)
+
+
+
+function send() {
+    if (check) {
+        let textCondition = document.getElementsByTagName('p')[0].innerText
+        document.getElementsByName('formulation')[0].value = textCondition;
+        document.getElementsByName('result')[0].value = result;
+        document.getElementById("UserEnter").submit();
+    } else {
+        alert("Есть недостатки. Повторите ввод")
+    }
+}
+
+function verify_send() {
+    verify();
+    send();
+}
+
+let messageText = document.getElementById("result").innerText
+console.log(messageText)
+let result;
+let check = false;
+
+
